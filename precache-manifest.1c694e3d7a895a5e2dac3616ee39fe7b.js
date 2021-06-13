8c7e6d93401bd2a58380076a8f00656ae20e91c4
Max <m.bogach@corp.mail.ru>
2021-06-13 16:08:33 +0300

    Updates

---
+++ precache-manifest.1c694e3d7a895a5e2dac3616ee39fe7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4926c60d5314e33e1e1f8406039cd1d",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "3afb6e3a44e8790d14b0",
+    "url": "/covid/static/css/main.31e58d3e.chunk.css"
+  },
+  {
+    "revision": "992c78de5ac1e59912f0",
+    "url": "/covid/static/js/2.fddc1ffd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.fddc1ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3afb6e3a44e8790d14b0",
+    "url": "/covid/static/js/main.32dce851.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  }
+]);
